4f6dd0bfc7aa5efa874fd08e74210d518997d70d
Daris Chen <daris.chen@gmail.com>
2026-06-18 12:39:23 -0700

    config for chromium

---
+++ client/snap.js
@@ -0,0 +1,38 @@
+module.exports = {
+  source: 'build',
+  puppeteerArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--single-process',
+    '--no-zygote',
+    '--disable-gpu'
+  ],
+  minifyHtml: {
+    collapseWhitespace: false,
+    removeComments: false
+  },
+  include: [
+    '/',
+    '/about',
+    '/projects',
+    '/contact',
+    '/resume',
+    '/projects/HB100',
+    '/projects/patentiq',
+    '/projects/chess',
+    '/projects/flipperzillow',
+    '/projects/simplerfy',
+    '/projects/groundworks',
+    '/projects/CPU',
+    '/projects/redshift',
+    '/projects/16bit-adder',
+    '/projects/weatherapp',
+    '/projects/eews',
+    '/projects/devblog',
+    '/projects/hearthpebble',
+    '/projects/darischen'
+  ],
+  puppeteer: {
+    executablePath: require('@sparticuz/chromium').executablePath
+  }
+};
